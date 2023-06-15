e37ef8c9b5f4a415ce5b5ddfd5d701ee24ee38f3
santosh <xantosh121@gmail.com>
2023-06-15 16:33:27 +0545

    addeed both gui and cli installer

--- baseline/airootfs/root/customize_airootfs.sh
+++ baseline/airootfs/root/customize_airootfs.sh
@@ -6,11 +6,11 @@
  pacman-key --populate
 #  pacman -Sy --noconfirm
 
-cp /root/kernel/vmlinuz-linux /boot
-rm /usr/lib/modules/* -rf
-cp /root/kernel/6.2.0* /usr/lib/modules/ -rf
-rm /root/kernel -rf
-mkinitcpio -p linux
+# cp /root/kernel/vmlinuz-linux /boot
+# rm /usr/lib/modules/* -rf
+# cp /root/kernel/6.2.0* /usr/lib/modules/ -rf
+# rm /root/kernel -rf
+# mkinitcpio -p linux
 
 
 echo "End of my customization"
